35cf45cc81bfe190baf5612b4b7bfb7d3d2586c3
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2025-09-10 16:34:01 +0800

    install tidewave

--- lib/elixir_chat_web/endpoint.ex
+++ lib/elixir_chat_web/endpoint.ex
@@ -26,6 +26,10 @@ defmodule ElixirChatWeb.Endpoint do
     gzip: not code_reloading?,
     only: ElixirChatWeb.static_paths()
 
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
